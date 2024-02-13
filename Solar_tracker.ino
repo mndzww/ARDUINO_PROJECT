f8a855890b4edd250c125d8092780eb32c535faf
mndzww <126949162+mndzww@users.noreply.github.com>
2024-02-13 16:21:17 +0700

    Update Solar_tracker.ino

--- Solar_tracker.ino
+++ Solar_tracker.ino
@@ -1,82 +1,61 @@
-#include <Servo.h> 
-Servo horizontal; // servo sumbu horizontal
-int servoh = 180; 
-int servohLimitHigh = 175;
-int servohLimitLow = 5;
 
+#include <Servo.h>
+#include <ESP8266wifi.h>
+#include <ESP8266WebServer.h>
 
-Servo vertical; // servi sumbu vertikal
-int servov = 45; 
-int servovLimitHigh = 100;
-int servovLimitLow = 1;
+Servo servoX;
+Servo servoY;
 
-// LDR pin connections
-// name = analogpin;
-int ldrlt = A0; //LDR top left - BOTTOM LEFT <--- BDG
-int ldrrt = A3; //LDR top rigt - BOTTOM RIGHT
-int ldrld = A1; //LDR down left - TOP LEFT
-int ldrrd = A2; //ldr down rigt - TOP RIGHT
+const int ldrLeftDown = A0;
+const int ldrLeftRight = A1;
+const int ldrUpLeft = A2;
+const int ldrUpRight = A3;
 
-void setup(){
-horizontal.attach(9);
-vertical.attach(10);
-horizontal.write(180);
-vertical.write(45);
-delay(2500);
+const char *ssid = "NamaWiFi";
+const char *password = "PasswordWiFi";
+
+ESP8266WebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+
+  servoX.attach(D5); // Hubungkan servo X ke pin D5 pada NodeMCU
+  servoY.attach(D6); // Hubungkan servo Y ke pin D6 pada NodeMCU
+
+  // Menghubungkan ke WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+
+  server.on("/", HTTP_GET, []() {
+    server.send(200, "text/plain", "Hello from ESP8266!");
+  });
+
+  server.begin();
 }
-void loop() {
-int lt = analogRead(ldrlt);
-int rt = analogRead(ldrrt);
-int ld = analogRead(ldrld);
-int rd = analogRead(ldrrd);
-int dtime = 10; int tol = 90;
-int avt = (lt + rt) / 2;
-int avd = (ld + rd) / 2;
-int avl = (lt + ld) / 2;
-int avr = (rt + rd) / 2;
-int dvert = avt - avd; 
-int dhoriz = avl - avr; 
 
-if (-1*tol > dvert || dvert > tol) 
- {
- if (avt > avd)
- {
- servov = ++servov;
- if (servov > servovLimitHigh)
- {servov = servovLimitHigh;}
- }
- else if (avt < avd)
- {servov= --servov;
- if (servov < servovLimitLow)
- { servov = servovLimitLow;}
- }
- vertical.write(servov);
- }
-if (-1*tol > dhoriz || dhoriz > tol) //pengecekan perbedaan toleransi disaat mengubah sudut horizontal
- {
- if (avl > avr)
- {
- servoh = --servoh;
- if (servoh < servohLimitLow)
- {
- servoh = servohLimitLow;
- }
- }
- else if (avl < avr)
- {
- servoh = ++servoh;
- if (servoh > servohLimitHigh)
- {
- servoh = servohLimitHigh;
- }
- }
- else if (avl = avr)
- {
- delay(5000);
- }
- horizontal.write(servoh);
- }
- 
- delay(dtime);
- 
+void loop() {
+  server.handleClient();
+
+  int ldrValues[4];
+  
+  ldrValues[0] = analogRead(ldrLeftDown);
+  ldrValues[1] = analogRead(ldrLeftRight);
+  ldrValues[2] = analogRead(ldrUpLeft);
+  ldrValues[3] = analogRead(ldrUpRight);
+
+  int deltaX = ldrValues[1] - ldrValues[0];
+  int deltaY = ldrValues[3] - ldrValues[2];
+
+  int angleX = map(deltaX, -1023, 1023, 0, 180);
+  int angleY = map(deltaY, -1023, 1023, 0, 180);
+
+  servoX.write(angleX);
+  servoY.write(angleY);
+
+  delay(1000); // Delay untuk stabilisasi
 }
